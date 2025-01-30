eabe95231fac4314a59f97a89bca85640822db83
kkholodova <karina_kholodova@list.ru>
2025-01-30 05:19:22 +0300

    Updates

--- pages/backend/app/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from .models import *
-
-admin.site.register(Airline)
-admin.site.register(Flight)
-admin.site.register(AirlineFlight)
--- pages/frontend/src/modules/types.ts
+++
@@ -1,8 +0,0 @@
-export type T_Airline =  {
-    id: number,
-    name: string,
-    description: string,
-    foundation_year: number,
-    image: string,
-    status: number
-}
